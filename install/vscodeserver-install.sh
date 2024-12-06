423e34e0f812c60eb54460a41c8b25f40bbc5d97
The Repos <ry4nyeo@gmail.com>
2024-12-07 02:37:59 +0800

    create vscodeserver script

---
+++ install/vscodeserver-install.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2024 tteck
+# Author: tteck (tteckster)
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y curl
+$STD apt-get install -y sudo
+$STD apt-get install -y mc
+msg_ok "Installed Dependencies"
+
+msg_info "Installing VS Code Server"
+curl -fsSL https://code-server.dev/install.sh | sh
+# Ensure binary is moved to /opt/code-server for consistent placement
+mkdir -p /opt/code-server
+cp $(which code-server) /opt/code-server/code-server
+msg_ok "Installed VS Code Server"
+
+msg_info "Creating Service"
+cat <<EOF >/etc/systemd/system/code-server.service
+[Unit]
+Description=VS Code Server
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/opt/code-server/code-server --host 0.0.0.0 --port 8081 --auth none --disable-telemetry
+WorkingDirectory=/opt/code-server
+Restart=always
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable -q --now code-server.service
+msg_ok "Created Service"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
+
+# [Service]
+# Type=simple
+# User=root
+# ExecStart=/usr/bin/code-server --host 0.0.0.0 --port 8081 --auth none --disable-telemetry
+# Restart=always
+# # Environment=PASSWORD=yourpassword
---
+++ vscodeserver.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
+# Copyright (c) 2021-2024 tteck
+# Author: tteck (tteckster)
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+
+function header_info {
+clear
+cat <<"EOF"
+                         | |                                   
+ __   _____  ___ ___   __| | ___  ___  ___ _ ____   _____ _ __ 
+ \ \ / / __|/ __/ _ \ / _` |/ _ \/ __|/ _ \ '__\ \ / / _ \ '__|
+  \ V /\__ \ (_| (_) | (_| |  __/\__ \  __/ |   \ V /  __/ |   
+   \_/ |___/\___\___/ \__,_|\___||___/\___|_|    \_/ \___|_|                            
+ 
+EOF
+}
+header_info
+echo -e "Loading..."
+APP="VSCodeServer"
+var_disk="2"
+var_cpu="2"
+var_ram="8192"
+var_os="debian"
+var_version="12"
+variables
+color
+catch_errors
+
+function default_settings() {
+  CT_TYPE="1"
+  PW=""
+  CT_ID=$NEXTID
+  HN=$NSAPP
+  DISK_SIZE="$var_disk"
+  CORE_COUNT="$var_cpu"
+  RAM_SIZE="$var_ram"
+  BRG="vmbr0"
+  NET="dhcp"
+  GATE=""
+  APT_CACHER=""
+  APT_CACHER_IP=""
+  DISABLEIP6="no"
+  MTU=""
+  SD=""
+  NS=""
+  MAC=""
+  VLAN=""
+  SSH="no"
+  VERB="no"
+  echo_default
+}
+
+function update_script() {
+header_info
+if [[ ! -d /opt/code-server ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+
+# Storage check
+if (( $(df /boot | awk 'NR==2{gsub("%","",$5); print $5}') > 80 )); then
+  read -r -p "Warning: Storage is dangerously low, continue anyway? <y/N> " prompt
+  [[ ${prompt,,} =~ ^(y|yes)$ ]] || exit
+fi
+
+# wget -qL https://static.adguard.com/adguardhome/release/AdGuardHome_linux_amd64.tar.gz
+msg_info "Stopping code-server service"
+systemctl stop code-server
+msg_ok "Stopped code-server service"
+
+msg_info "Updating code-server"
+curl -fsSL https://code-server.dev/install.sh | sh || {
+    msg_error "Failed to update code-server";
+    exit
+}
+msg_ok "Updated code-server"
+
+msg_info "Starting code-server service"
+systemctl start code-server || msg_error "Failed to start code-server"; exit
+msg_ok "Started code-server service"
+
+msg_info "Cleaning Up"
+rm -rf /tmp/code-server-installation-files  # Adjust if temp files are used
+msg_ok "Cleaned up installation files"
+msg_ok "VS Code Server Updated Successfully"
+exit
+
+}
+
+# Calls build.func
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${APP} Setup should be reachable by going to the following URL.
+         ${BL}http://${IP}:8081${CL} \n"
