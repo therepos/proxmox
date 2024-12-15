55255a9b8a7bf619bb590d782681dd4a78b26dd8
The Repos <ry4nyeo@gmail.com>
2024-12-15 14:15:59 +0800

    Update install-nvidiact.sh

--- install-nvidiact.sh
+++ install-nvidiact.sh
@@ -5,8 +5,7 @@
 
 # Add the NVIDIA package repositories
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-   && curl -s -L https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
-   && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/nvidia-container-toolkit.list | \
+   && curl -s -L https://nvidia.github.io/libnvidia-container/debian12/nvidia-container-toolkit.list | \
          sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
          sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
