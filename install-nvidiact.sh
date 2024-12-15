d893b4b8dad49102a4ee0aa4ee6862d6fe92ef10
The Repos <ry4nyeo@gmail.com>
2024-12-15 14:25:04 +0800

    Update install-nvidiact.sh

--- install-nvidiact.sh
+++ install-nvidiact.sh
@@ -3,12 +3,21 @@
 # wget --no-cache -qO- https://raw.githubusercontent.com/therepos/proxmox/main/install-nvidiact.sh | bash
 # curl -fsSL https://raw.githubusercontent.com/therepos/proxmox/main/install-nvidiact.sh | bash
 
-# Add the NVIDIA package repositories
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-   && curl -s -L https://nvidia.github.io/libnvidia-container/debian12/nvidia-container-toolkit.list | \
-         sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
-         sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+# Error detection: script halts execution as soon as it encounters a failure.
+set -e
 
-# Update the package list and install the NVIDIA Container Toolkit
-sudo apt-get update
-sudo apt-get install -y nvidia-container-toolkit
+# Detect architecture
+ARCH=$(dpkg --print-architecture)
+
+# Add NVIDIA GPG key
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
+
+# Add NVIDIA repository
+curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+  sed "s#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g" | \
+  sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+# Update and install NVIDIA Container Toolkit
+sudo apt update
+sudo apt install -y nvidia-container-toolkit
+
