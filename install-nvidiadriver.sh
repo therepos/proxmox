5b3e06b0bc3b75e09bec4ee2f833c88917d6340e
The Repos <ry4nyeo@gmail.com>
2024-12-15 12:14:41 +0800

    Update install-nvidiadriver.sh

--- install-nvidiadriver.sh
+++ install-nvidiadriver.sh
@@ -5,8 +5,11 @@ echo "blacklist nouveau" | sudo tee /etc/modprobe.d/blacklist-nouveau.conf
 echo "options nouveau modeset=0" | sudo tee -a /etc/modprobe.d/blacklist-nouveau.conf
 sudo update-initramfs -u
 
+# Append to end of file
+echo 'export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/lib/x86_64-linux-gnu/pkgconfig/' >> ~/.bashrc
+
 # Install dependencies
-sudo apt install -y build-essential pve-headers-$(uname -r) pkg-config libx11-dev libxext-dev xorg-dev xserver-xorg-core xserver-xorg-dev lib32z1
+sudo apt install -y build-essential pve-headers-$(uname -r) pkg-config libglvnd-dev libx11-dev libxext-dev xorg-dev xserver-xorg-core xserver-xorg-dev lib32z1
 
 # Download NVIDIA driver
 wget https://us.download.nvidia.com/XFree86/Linux-x86_64/550.135/NVIDIA-Linux-x86_64-550.135.run
@@ -15,7 +18,8 @@ wget https://us.download.nvidia.com/XFree86/Linux-x86_64/550.135/NVIDIA-Linux-x8
 chmod +x NVIDIA-Linux-x86_64-550.135.run
 
 # Run the driver installation script
-sudo ./NVIDIA-Linux-x86_64-550.135.run
+sudo ./NVIDIA-Linux-x86_64-550.135.run --glvnd-egl-config-path=/etc/glvnd/egl_vendor.d
+# sudo ./NVIDIA-Linux-x86_64-550.135.run -a -s -N --no-drm --log-file=/tmp/nvidia-installer.log
 
 # Update and install CUDA keyring
 sudo apt update
@@ -29,11 +33,17 @@ nvidia-smi
 apt-get remove --purge '^nvidia-.*'
 apt-get autoremove -y 
 
+#
 sudo apt install nvidia-driver
 
+#
 wget https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/libnvidia-container1_1.17.3-1_amd64.deb
 wget https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/nvidia-container-toolkit_1.17.3-1_amd64.deb
 sudo dpkg -i libnvidia-container1_1.17.3-1_amd64.deb
 sudo dpkg -i nvidia-container-toolkit_1.17.3-1_amd64.deb
+
+#
+sudo ./NVIDIA-Linux-x86_64-550.135.run --uninstall
+
 EOF
 
