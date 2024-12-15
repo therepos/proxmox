14f2b49047de579c6283c3cd7840db28a59c4b87
The Repos <ry4nyeo@gmail.com>
2024-12-16 03:16:45 +0800

    update installers

--- install-vscodeserver.sh
+++ install-vscodeserver.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# bash -c "$(wget --no-cache -qLO - https://github.com/therepos/proxmox/raw/main/vscodeserver.sh)"
-# bash -c "$(curl -fsSL https://github.com/therepos/proxmox/raw/main/vscodeserver.sh)"
+# bash -c "$(wget --no-cache -qLO - https://github.com/therepos/proxmox/raw/main/install-vscodeserver.sh)"
+# bash -c "$(curl -fsSL https://github.com/therepos/proxmox/raw/main/install-vscodeserver.sh)"
 # Appsource: https://coder.com/docs/code-server/install#installsh
 # Art: https://patorjk.com/software/taag/#p=display&f=Big&t=vscodeserver 
 # Modified by: therepos
