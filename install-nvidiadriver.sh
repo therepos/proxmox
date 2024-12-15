1bdb77e7a3c89ef0237bc0639a4f99e42e023cff
The Repos <ry4nyeo@gmail.com>
2024-12-15 11:53:35 +0800

    Create install-nvidiadriver.sh

---
+++ install-nvidiadriver.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Blacklist nouveau driver
+echo "blacklist nouveau" | sudo tee /etc/modprobe.d/blacklist-nouveau.conf
+echo "options nouveau modeset=0" | sudo tee -a /etc/modprobe.d/blacklist-nouveau.conf
+sudo update-initramfs -u
+
+# Install dependencies
+sudo apt install -y build-essential pve-headers-$(uname -r) pkg-config libx11-dev libxext-dev xorg-dev xserver-xorg-core xserver-xorg-dev lib32z1
+
+# Download NVIDIA driver
+wget https://us.download.nvidia.com/XFree86/Linux-x86_64/550.135/NVIDIA-Linux-x86_64-550.135.run
+
+# Make the driver script executable
+chmod +x NVIDIA-Linux-x86_64-550.135.run
+
+# Run the driver installation script
+sudo ./NVIDIA-Linux-x86_64-550.135.run
+
+# Update and install CUDA keyring
+sudo apt update
+sudo apt install -y cuda-keyring
+
+# Verify 
+nvidia-smi
+
+: <<'EOF'
+#The -y flag skips confirmation for autoremove.
+apt-get remove --purge '^nvidia-.*'
+apt-get autoremove -y 
+
+sudo apt install nvidia-driver
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/libnvidia-container1_1.17.3-1_amd64.deb
+wget https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/nvidia-container-toolkit_1.17.3-1_amd64.deb
+sudo dpkg -i libnvidia-container1_1.17.3-1_amd64.deb
+sudo dpkg -i nvidia-container-toolkit_1.17.3-1_amd64.deb
+EOF
+
