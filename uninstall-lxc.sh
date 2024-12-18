d3196f3988d274190238a514b6bc6bc5aa86a5a5
The Repos <ry4nyeo@gmail.com>
2024-12-18 19:20:34 +0800

    Update uninstall-lxc.sh

--- uninstall-lxc.sh
+++ uninstall-lxc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# wget --header="Cache-Control: no-cache" -qO- https://raw.githubusercontent.com/therepos/proxmox/main/uninstall-lxc.sh | bash
+# wget -qO- https://raw.githubusercontent.com/therepos/proxmox/main/uninstall-lxc.sh | bash
 # curl -fsSL https://raw.githubusercontent.com/therepos/proxmox/main/uninstall-lxc.sh | bash
 
 #!/bin/bash
