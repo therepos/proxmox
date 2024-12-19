69c3e12e294a15658b27378ad68cebdbd0d54814
The Repos <ry4nyeo@gmail.com>
2024-12-19 14:21:24 +0800

    Update install-nvidiact.sh

--- installers/install-nvidiact.sh
+++ installers/install-nvidiact.sh
@@ -10,15 +10,15 @@ set -e
 ARCH=$(dpkg --print-architecture)
 
 # Add NVIDIA GPG key
-curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
 
 # Add NVIDIA repository
 curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
   sed "s#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g" | \
-  sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+  tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
 # Update and install NVIDIA Container Toolkit
-sudo apt update
-sudo apt install -y nvidia-container-toolkit
+apt update
+apt install -y nvidia-container-toolkit
 
 # References: https://medium.com/@u.mele.coding/a-beginners-guide-to-nvidia-container-toolkit-on-docker-92b645f92006
