536826ee9aa5d712736bf6dca2eb211e423337da
The Repos <ry4nyeo@gmail.com>
2024-12-23 01:57:27 +0800

    Update install-nvidiact.sh

--- installers/install-nvidiact.sh
+++ installers/install-nvidiact.sh
@@ -9,6 +9,9 @@ set -e
 # Detect architecture
 ARCH=$(dpkg --print-architecture)
 
+# Remove redundant file
+rm /etc/apt/sources.list.d/cuda-debian12-x86_64.list
+
 # Add NVIDIA GPG key
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
   && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
