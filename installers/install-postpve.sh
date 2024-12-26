e859bb5043b752e86836db05f368a3421eaaa17b
The Repos <ry4nyeo@gmail.com>
2024-12-27 02:32:03 +0800

    Update install-postpve.sh

--- installers/install-postpve.sh
+++ installers/install-postpve.sh
@@ -210,7 +210,7 @@ echo -e "\nThis script will Perform Post Install Routines.\n"
 echo "Starting Post Install Script automatically..."
 
 
-if ! pveversion | grep -Eq "pve-manager/8.[0-2]"; then
+if ! pveversion | grep -Eq "pve-manager/8.[0-3]"; then
   msg_error "This version of Proxmox Virtual Environment is not supported"
   echo -e "Requires Proxmox Virtual Environment Version 8.0 or later."
   echo -e "Exiting..."
