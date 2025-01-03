11de5b2f9318e1e144052a1334d2ec69e084df03
The Repos <ry4nyeo@gmail.com>
2025-01-03 09:53:02 +0800

    Update sambashare.sh

--- util/sambashare.sh
+++ util/sambashare.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# bash -c "$(wget -qLO- https://github.com/therepos/proxmox/raw/main/util/sambashare.sh)"
+
 # Define colors and status symbols
 GREEN="\e[32m✔\e[0m"
 RED="\e[31m✘\e[0m"
