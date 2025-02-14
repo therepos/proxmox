912092cddd7e2121599a915b668756edbbd0fbce
The Repos <ry4nyeo@gmail.com>
2025-02-14 14:24:35 +0800

    Update install-samba.sh

--- apps/install-samba.sh
+++ apps/install-samba.sh
@@ -2,9 +2,6 @@
 # bash -c "$(wget -qLO- https://github.com/therepos/proxmox/raw/main/apps/install-samba.sh)"
 # purpose: this script installs samba
 
-#!/bin/bash
-# Enhanced Samba installation script with user existence checks
-
 # Define colors and status symbols
 GREEN="\e[32m\u2714\e[0m"
 RED="\e[31m\u2718\e[0m"
