09efb044769f13d1474b3716078d09fd9eef7967
The Repos <ry4nyeo@gmail.com>
2025-02-16 18:56:10 +0800

    Update backup-winstorage.sh

--- tools/backup-winstorage.sh
+++ tools/backup-winstorage.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # bash -c "$(wget -qLO- https://github.com/therepos/proxmox/raw/main/tools/backup-winstorage.sh)"
+# purpose: this script backups storage folder in a docker container called windows
 
 # Source and destination directories
 SOURCE_DIR="/mnt/sec/apps/windows/storage/"
