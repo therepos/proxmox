7a94e343e113a37a6afd29844b4873c457db8bee
The Repos <ry4nyeo@gmail.com>
2025-03-03 01:55:56 +0800

    Update install-mediacms.sh

--- apps/install-mediacms.sh
+++ apps/install-mediacms.sh
@@ -40,7 +40,7 @@ fi
 
 # Add volume mount for `/mnt/sec/media/temp`
 if sed -i '/web:/,/volumes:/s|\(volumes:\)|\1\n      - /mnt/sec/media/temp:/mnt/sec/media/temp|' docker-compose.yaml; then
-    status_message "success" "Mounted /mnt/sec/media/videos to /media_files/videos."
+    status_message "success" "Mounted /mnt/sec/media/temp."
 else
     status_message "error" "Failed to mount media volume."
 fi
