c09048174856207f8f2e0390f8e6b4730f2bccab
The Repos <ry4nyeo@gmail.com>
2025-06-02 23:33:12 +0800

    updates

--- tools/docgen-compose.py
+++ tools/docgen-compose.py
@@ -10,13 +10,13 @@ def generate_index():
     GITHUB_RAW_BASE = "https://raw.githubusercontent.com/therepos/proxmox/main/docker"
     # GITHUB_VIEW_BASE = "https://github.com/therepos/proxmox/blob/main/docker"
 
-    lines = ['# Templates\n']
+    lines = ['# Templates\n', '## Docker\n']
     for filename in sorted(os.listdir(DOCKER_DIR)):
         if filename.endswith('-docker-compose.yml'):
             filepath = os.path.join(DOCKER_DIR, filename)
             raw_url = f"{GITHUB_RAW_BASE}/{filename}"
             servicename = filename.split('-')[0].capitalize()
-            lines.append(f"- [{servicename}]({raw_url})\n")
+            lines.append(f"•&nbsp;&nbsp;[{servicename}]({raw_url})  \n")
 
     with open(OUTPUT_MD, 'w') as out:
         out.write('\n'.join(lines))
