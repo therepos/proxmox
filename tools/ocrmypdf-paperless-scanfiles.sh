49a55f54b2f51d71edc2098770cd906f07de107a
The Repos <ry4nyeo@gmail.com>
2025-06-03 07:47:51 +0800

    Update ocrmypdf-paperless-scanfiles.sh

--- tools/ocrmypdf-paperless-scanfiles.sh
+++ tools/ocrmypdf-paperless-scanfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # bash -c "$(wget -qLO- https://github.com/therepos/proxmox/raw/main/tools/ocrmypdf-scanfiles.sh?$(date +%s))"
-# purpose: ocr scans files in the input folder to the output folder
+# purpose: ocr scans files from the input folder to the output folder
 
 INPUT_DIR="/mnt/sec/apps/ocrmypdf/input"
 OUTPUT_DIR="/mnt/sec/apps/ocrmypdf/output"
