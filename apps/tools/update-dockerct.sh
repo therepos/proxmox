fa67f8b0a83fe9f47bbfe57ff6b314a7e87a95f6
The Repos <ry4nyeo@gmail.com>
2025-06-13 01:42:34 +0800

    updates

---
+++ apps/tools/update-dockerct.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# bash -c "$(wget -qLO- https://github.com/therepos/proxmox/raw/main/apps/tools/set-nonroot.sh?$(date +%s))"
+# purpose: update all Docker Compose apps
+
+BASE_DIR="/mnt/sec/apps"
+
+for dir in "$BASE_DIR"/*; do
+  if [ -f "$dir/docker-compose.yml" ]; then
+    echo "Updating in $dir..."
+    (cd "$dir" && docker-compose pull && docker-compose up -d)
+  fi
+done
+
+# cleanup
+docker image prune -f
