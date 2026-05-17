47a264d8bbed53c0a709b4cac5a3a1a69b8a70ad
The Repos <ry4nyeo@gmail.com>
2026-05-18 04:25:41 +0800

    up

--- .scraps/mount-drive-v2.sh
+++ .scraps/mount-drive-v2.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# bash -c "$(wget -qLO- https://github.com/therepos/proxmox/raw/main/apps/tools/mount-drive-v2.sh?$(date +%s))"
+# bash -c "$(wget -qLO- https://github.com/therepos/proxmox/raw/main/scraps/mount-drive-v2.sh?$(date +%s))"
 # purpose: mounts a user-specified drive and updates fstab
 
 # Colors for output
